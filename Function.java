24674124be90c5bc11d2c571a6a7b07601f0f49f
Jaskaran Singh <89383365+Jaskaran-Techno@users.noreply.github.com>
2024-12-25 20:09:20 +0530

    Create Function.java

---
+++ Function.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+    class Pack {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int  i = scn.nextInt();
+        for(int n = 1;n<=5;n++)
+        {
+            System.out.println(i);
+        }
+    }
+}
