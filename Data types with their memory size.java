0db606d32cc98ff924e9d5f7cc7ece0a88dd6355
Jaskaran Singh <89383365+Jaskaran-Techno@users.noreply.github.com>
2025-01-29 17:05:48 +0530

    Create Data types with their memory size.java

---
+++ Data types with their memory size.java
@@ -0,0 +1,13 @@
+1 byte = 8 bits
+1 GB = 1024 MB
+1 MB = 1024 KB
+1 KB = 1024 bytes
+// Data Types with thier respective memory size.
+
+Byte - 1 byte
+char - 2 byte
+boolean - 1 byte
+int - 4 byte
+long - 8 byte
+float - 4 byte
+double - 8 byte
