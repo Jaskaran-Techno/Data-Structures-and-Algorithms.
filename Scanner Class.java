02d678538d055234320e353058d061088178bdaf
Jaskaran Singh <89383365+Jaskaran-Techno@users.noreply.github.com>
2025-02-03 17:22:23 +0530

    Create Scanner Class.java

---
+++ Scanner Class.java
@@ -0,0 +1,13 @@
+package com.comapany;
+
+import java.util.*;
+
+public class Main {
+
+public static void main(String [] args) {
+
+Scanner sc = new Scanner(System.in);
+String name = sc.next();
+System.out.println(name);
+                                        }
+                  }
